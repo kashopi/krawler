56237ae773507f388849047ee36f17a8a702ca6d
Juan I. Rodriguez <kashopi@gmail.com>
2018-02-11 16:40:20 +0100

    Adapt main to pubsub

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import logging
-
+import redis
 from cache.redis import RedisCache
 from crawler import Crawler
 
 
 logging.basicConfig(level=logging.INFO)
-crawler = Crawler(RedisCache())
-# page = crawler.get_page("https://google.es")
-page = crawler.get_page("https://www.simracingcoach.com/")
-links = crawler.get_links(page)
-logging.info("Found %d links", len(links.keys()))
+
+redis_connection = redis.StrictRedis(host='localhost', port=6379, db=0)
+crawler = Crawler(RedisCache(redis_connection), redis_connection)
+crawler.start()
